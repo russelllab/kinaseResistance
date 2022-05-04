12326b03de1f98011721bac745aa8f7021551b38
gurdeep330 <gurdeep330@gmail.com>
2022-05-04 15:53:15 +0200

    generate HETATM

--- data/extract_non_polymer_entity.py
+++ data/extract_non_polymer_entity.py
@@ -4,24 +4,22 @@
 import Bio.PDB
 import gzip, os, sys, threading
 
-threads = 2
+threads = 20
+verbose = 1
 
 ## Multithreading
 class myThread (threading.Thread):
    def __init__(self, threadID, pdb):
-       threading.Thread.__init__(self, pdb)
+       threading.Thread.__init__(self)
        self.threadID = threadID
        self.pdb = pdb
+
    def start(self):
        if verbose == 1:
-           print ("Threading " + self.name)
-       #else:
-           #print ('.', end='', flush=True)
+           print ("Threading", self.threadID)
        parseCIF(self.pdb)
        if verbose == 1:
-           print ("Exiting " + self.pdb)
-       #else:
-           #print ('.', end='', flush=True)
+           print ("Exiting", self.threadID)
 
 def call_main(thread, count, pdb):
     thread[count] = myThread(count, pdb)
@@ -36,35 +34,36 @@ for line in open('PF07714_pdb_chain_pfam.tsv', 'r'):
 pdbs = list(set(pdbs))
 print (len(pdbs))
 
-def parseCIF(pdb)
-	os.system('gunzip '+pdb+'.cif.gz')
+dic ={}
+def parseCIF(pdb):
 	parser = Bio.PDB.MMCIF2Dict.MMCIF2Dict(pdb+'.cif')
 	os.system('rm -rf '+pdb+'.cif')
-	if '_pdbx_entity_nonpoly.comp_id' in dic[pdb]:
-	    #print(dic[pdb]['_pdbx_entity_nonpoly.name'])
-	    #print (dic[pdb]['_pdbx_entity_nonpoly.comp_id'])
-	    for compid, compname in zip(parser['_pdbx_entity_nonpoly.comp_id'], parser['_pdbx_entity_nonpoly.name']):
-	       	#l += pdb + '\t' + compid + '\t' + compname + '\n'
-		dic[pdb][compid] = compname
-	num += 1
-	if (num == 100):
-		break
-    #sys.exit()
+	if '_pdbx_entity_nonpoly.comp_id' in parser:
+		dic[pdb] = {}
+		#print(dic[pdb]['_pdbx_entity_nonpoly.name'])
+		#print (dic[pdb]['_pdbx_entity_nonpoly.comp_id'])
+		for compid, compname in zip(parser['_pdbx_entity_nonpoly.comp_id'], parser['_pdbx_entity_nonpoly.name']):
+			#l += pdb + '\t' + compid + '\t' + compname + '\n'
+			dic[pdb][compid] = compname
 
-l = ''; count = 0
-dic = {}
+count = 0
+thread = {}
 pathToPDB = '/net/home.isilon/ds-russell/pdb-cif/'
 for pdb in pdbs:
 	code = pdb[1:-1]
 	if os.path.isfile(pathToPDB+code+'/'+pdb+'.cif.gz') == True:
-		os.system('cp '+pathToPDB+code+'/'+pdb+'.cif.gz .')
-	        os.system('gunzip '+pdb+'.cif.gz')
-		call_main(thread, count, pdb)
-		count += 1
-	        if (count == 100):
-        	        break
+                os.system('cp '+pathToPDB+code+'/'+pdb+'.cif.gz .')
+                os.system('gunzip '+pdb+'.cif.gz')
+                call_main(thread, count, pdb)
+                count += 1
+                #if (count == 100):
+                #    break
 
 while threading.activeCount() > 1:
     pass
 
+l = 'PDB\tCompID\tCompName\n'
+for pdb in dic:
+	for compid in dic[pdb]:
+		l += pdb + '\t' + compid + '\t' + dic[pdb][compid] + '\n'
 open('HETATM.tsv', 'w').write(l)
