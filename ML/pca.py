eb9196e860154d0985ed5b5eb5cc531caa63eaf2
gurdeep330 <[gurdeep330@gmail.com]>
2023-03-06 15:44:30 +0100

    update pca plot

--- ML/pca.py
+++ ML/pca.py
@@ -9,27 +9,45 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 from sklearn import decomposition
+from sklearn.preprocessing import MinMaxScaler
+import plotly.express as px
 
+df = pd.read_csv('trainData.tsv.gz', sep = '\t')
+# exclude columns
+# df = df.loc[:, ~df.columns.isin(['allHomologs','exclParalogs','specParalogs','orthologs', 'bpso','bpsh'])]
+df = df.loc[:, ~df.columns.isin([
+                            'allHomologs',
+                            'exclParalogs',
+                            'specParalogs',
+                            # 'orthologs',
+                            'bpso',
+                            'bpsh'
+                            ])]
+# exclude columns to make the data matrix
+original_df = df.copy()
+df = df.loc[:, ~df.columns.isin(['Acc', 'Mutation'])]
+print (df.to_numpy())
+# sys.exit()
 data = []
 mut_types_colors = []
-for line in gzip.open('trainData.tsv.gz', 'rt'):
-    if line.split('\t')[0] == 'Acc':
-        continue
-    row = [float(item) for item in line.split('\t')[2:-1]]
-    mut_type = line.replace('\n', '').split('\t')[-1]
+# for line in gzip.open('trainData.tsv.gz', 'rt'):
+for row in df.to_numpy():
+    mut_type = row[-1]
     if mut_type == 'A':
+        # continue
         mut_types_colors.append('green')
     elif mut_type == 'D':
         mut_types_colors.append('red')
     else:
-        continue
+        # continue
         mut_types_colors.append('violet')
-    data.append(row)
+    data.append(row[:-1])
 
-data = np.array(data)
+# data = np.array(data)
 # scaler = MinMaxScaler()
 # scaler.fit(data)
 # data = scaler.transform(data)
+# print (data[:,0])
 # print (trainMat)
 # sys.exit()
 
@@ -55,5 +73,21 @@ kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
 print (kmeans.labels_)
 # kmeans.predict([[0, 0], [12, 3]])
 print (kmeans.cluster_centers_)
+
+pca_df = pd.DataFrame(X, columns = ['PCA1', 'PCA2'])
+pca_df = pd.concat([pca_df, original_df], axis=1)
+print (pca_df)
+# print (original_df)
 plt.show()
+
+fig = px.scatter(
+                pca_df, x="PCA1", y="PCA2", color="MUT_TYPE",
+                 hover_data=['Acc', 'Mutation', 'p', 'p_pfam'],
+                 color_discrete_map={
+                                    "A": "green",
+                                    "D": "red",
+                                    "R": "blue"
+                                    }
+                 )
+fig.show()
 # plt.savefig('pca_plot.png')
