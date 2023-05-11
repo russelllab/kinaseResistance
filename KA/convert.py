0e835c1fb1cf824a88a8ad41dd128bd862fca9b0
gurdeep330 <[gurdeep330@gmail.com]>
2023-05-11 21:59:10 +0200

    update

--- KA/convert.py
+++
@@ -1,5 +0,0 @@
-from Bio import SeqIO
-
-records = SeqIO.parse("muscleAlignment2.fasta", "fasta")
-count = SeqIO.write(records, "muscleAlignment2.clustal", "clustal")
-print("Converted %i records" % count)
