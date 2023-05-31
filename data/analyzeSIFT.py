f0634d393ade1132842099d04bb226dccedf02ea
gurdeep330 <[gurdeep330@gmail.com]>
2023-05-31 19:29:21 +0200

    update analyze PMUT

--- data/analyzeSIFT.py
+++ data/analyzeSIFT.py
@@ -17,6 +17,7 @@ class Mutation:
         self.mutAA = mutAA
         self.mut_type = mut_type
         self.prediction = None
+        self.prob = None
 
 mydb = fetchData.connection(db_name='kinase_project2')
 mydb.autocommit = True
@@ -51,12 +52,14 @@ for name in dic_mutations:
         if line.startswith('UniProtID'): continue
         mutation = line.split('\t')[1].strip()+line.split('\t')[2].strip()+line.split('\t')[3].strip()
         siftPred = 'damaging' if 'True' in line.strip().split('\t')[19] else 'benign'
+        siftprob = float(line.strip().split('\t')[19].split('(')[1].split(')')[0])
         sift_dic[acc][mutation] = siftPred
         #print (acc+'/'+mutation)
         if acc+'/'+mutation in dic_mutations:
                 dic_mutations[acc+'/'+mutation].prediction = siftPred
+                dic_mutations[acc+'/'+mutation].prob = siftProb
 
-y_pred = []; y_true = []
+y_pred = []; y_true = []; y_prob=[]
 for mutation in dic_mutations:
     if dic_mutations[mutation].prediction is None: continue
     if dic_mutations[mutation].mut_type in ['resistance']: continue
@@ -65,10 +68,16 @@ for mutation in dic_mutations:
         y_pred.append(1)
     else:
         y_pred.append(0)
+    y_prob.append(dic_mutations[mutation].prob)
     if dic_mutations[mutation].mut_type in ['increase', 'activating', 'decrease', 'loss', 'resistance']:
         y_true.append(1)
     else:
         y_true.append(0)
+
+text = ''
+for y_p, y_t in zip(y_prob, y_true):
+    text += str(y_p) + '\t' + str(y_t) + '\n'
+open('pmut_roc.txt', 'w').write(text)
 
 # print (y_pred)
 print (f'MCC: {matthews_corrcoef(y_true, y_pred)}')
