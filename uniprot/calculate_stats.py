4703287424828efcbb70c1bada7515d91ba8f72e
JCGonzs <juan-carlos.gonzalez@bioquant.uni-heidelberg.de>
2023-06-23 16:35:30 +0200

    clean code

--- uniprot/calculate_stats.py
+++ uniprot/calculate_stats.py
@@ -1,33 +1,46 @@
+import sys
+import os
 import gzip
+import csv
 import pandas as pd
+import numpy as np
 
 
-if __name__ == '__main__':
-    file_path = {
-        "activark": "uniprot_muts_activark.txt.gz",
-        "uniprot": "allUniProtVariants.txt",
-        "merged_file": "activark_uniprot_merged.tsv",
-        "stats": "stats_from_uniprot_activark.txt"
-    }
+def merge_predictions_with_uniprot_annotations(path):
+    """
+    Combine Activark predictions with UniProt annotations.
+    """
+    if os.path.exists(path["merged_file"]):
+        return pd.read_csv(path["merged_file"], sep="\t", compression="gzip")
+    else:
+        df_act = pd.read_csv(path["activark"], sep=r"\s+", comment='#')
+        df_uni = pd.read_csv(path["uniprot"], sep="\t", comment='#')
+        df_uni.rename(columns={"UniProt/Mutation": "UserInput"}, inplace=True)
+        df = pd.merge(df_act, df_uni[["UserInput", "Type", "Note", "Pubmed"]],
+                      on="UserInput", how="left")
+        df["n_PMIDs"] = df["Pubmed"].str.count(",") + 1
+        df["n_PMIDs"] = df["n_PMIDs"].replace(np.inf, 0).replace(np.nan, 0)
+        df["n_PMIDs"] = df["n_PMIDs"].astype('int')
+        df.to_csv(path["merged_file"], sep="\t", index=False, compression="gzip")
+    return df
 
-    df_act = pd.read_csv(file_path["activark"], sep=r"\s+", comment='#')
-    df_uni = pd.read_csv(file_path["uniprot"], sep="\t", comment='#')
-    df_uni.rename(columns={"UniProt/Mutation": "UserInput"}, inplace=True)
-    df = pd.merge(df_act, df_uni[["UserInput", "Type", "Note", "Pubmed"]], 
-                  on="UserInput", how="left")
-    df.to_csv(file_path["merged_file"], sep="\t", index=False)
 
-    final_df = []
-    final_df.append({
+def compute_stats(df, output_file):
+    """
+    Compute statistics for the given dataframe.
+    """
+    final_df = [{
         "Set": "ALL",
         "Total_Vars": str(len(df.index)),
         "Total_Prots": str(len(df["UniProtAcc"].unique())),
 
-        "KnownA_Vars": str(len(df[df["KnownADR"].str.contains("activating") | df["KnownADR"].str.contains("increase")].index)),
+        "KnownA_Vars": str(
+            len(df[df["KnownADR"].str.contains("activating") | df["KnownADR"].str.contains("increase")].index)),
         "KnownA_%": f"{round(len(df[df['KnownADR'].str.contains('activating') | df['KnownADR'].str.contains('increase')].index) / len(df.index) * 100, 2)}%",
         "KnownR_Vars": str(len(df[df["KnownADR"].str.contains("resistance")].index)),
         "KnownR_%": f"{round(len(df[df['KnownADR'].str.contains('resistance')].index) / len(df.index) * 100, 2)}%",
-        "KnownD_Vars": str(len(df[df["KnownADR"].str.contains("decrease") | df["KnownADR"].str.contains("loss")].index)),
+        "KnownD_Vars": str(
+            len(df[df["KnownADR"].str.contains("decrease") | df["KnownADR"].str.contains("loss")].index)),
         "KnownD_%": f"{round(len(df[df['KnownADR'].str.contains('decrease') | df['KnownADR'].str.contains('loss')].index) / len(df.index) * 100, 2)}%",
         "KnownN_Vars": str(len(df[df["KnownADR"].str.contains("neutral")].index)),
         "KnownN_%": f"{round(len(df[df['KnownADR'].str.contains('neutral')].index) / len(df.index) * 100, 2)}%",
@@ -38,7 +51,7 @@ if __name__ == '__main__':
         "PredD_%": f"{round(len(df[df['AIvLD'] <= 0.5].index) / len(df.index) * 100, 2)}%",
         "PredR_Vars": str(len(df[df["RvN"] > 0.5].index)),
         "PredR_%": f"{round(len(df[df['RvN'] > 0.5].index) / len(df.index) * 100, 2)}%",
-    })
+    }]
 
     for type in ["MUTAGEN", "VARIANT"]:
         df2 = df[df["Type"] == type]
@@ -65,4 +78,17 @@ if __name__ == '__main__':
         })
 
     final_df = pd.DataFrame(final_df)
-    final_df.to_csv(file_path["stats"], sep="\t", index=False)
+    final_df.to_csv(output_file, sep="\t", index=False)
+
+
+if __name__ == '__main__':
+    file_path = {
+        "activark": "uniprot_muts_activark.txt.gz",
+        "uniprot": "allUniProtVariants.txt",
+        "merged_file": "uniprot_muts_activark_with_annotations.txt.gz",
+        "stats": "stats_from_uniprot_activark.txt",
+    }
+
+    df = merge_predictions_with_uniprot_annotations(file_path)
+
+    compute_stats(df, file_path["stats"])
