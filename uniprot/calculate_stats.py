fe4e8a113c3c03f293e6ec1775a2afc92b177644
JCGonzs <juan-carlos.gonzalez@bioquant.uni-heidelberg.de>
2023-06-29 02:32:42 +0200

    update; close #61

--- uniprot/calculate_stats.py
+++ uniprot/calculate_stats.py
@@ -55,7 +55,7 @@ def compute_stats(df, output_file):
 
     for type in ["MUTAGEN", "VARIANT"]:
         df2 = df[df["Type"] == type]
-        final_df.append({
+        d ={
             "Set": type,
             "Total_Vars": str(len(df2.index)),
             "Total_Prots": str(len(df2["UniProtAcc"].unique())),
@@ -68,14 +68,26 @@ def compute_stats(df, output_file):
             "KnownD_%": f"{round(len(df2[df2['KnownADR'].str.contains('decrease') | df2['KnownADR'].str.contains('loss')].index) / len(df2.index) * 100, 2)}%",
             "KnownN_Vars": str(len(df2[df2["KnownADR"].str.contains("neutral")].index)),
             "KnownN_%": f"{round(len(df2[df2['KnownADR'].str.contains('neutral')].index) / len(df2.index) * 100, 2)}%",
+        }
+        df_a = df2[(df2["A"] > df2["D"]) & (df2["A"] > df2["N"]) & (df2["AIvLD"] > 0.5)]
+        df_d = df2[(df2["D"] > df2["A"]) & (df2["D"] > df2["N"]) % (df2["AIvLD"] <= 0.5)]
+        df_n = df2[(df2["N"] > df2["A"]) & (df2["N"] > df2["D"])]
+        df_r = df2[df2["RvN"] > 0.5]
 
-            "PredA_Vars": str(len(df2[df2["AIvLD"] > 0.5].index)),
-            "PredA_%": f"{round(len(df2[df2['AIvLD'] > 0.5].index) / len(df2.index) * 100, 2)}%",
-            "PredD_Vars": str(len(df2[df2["AIvLD"] <= 0.5].index)),
-            "PredD_%": f"{round(len(df2[df2['AIvLD'] <= 0.5].index) / len(df2.index) * 100, 2)}%",
-            "PredR_Vars": str(len(df2[df2["RvN"] > 0.5].index)),
-            "PredR_%": f"{round(len(df2[df2['RvN'] > 0.5].index) / len(df2.index) * 100, 2)}%",
-        })
+        d["PredA_Vars"] = str(len(df_a.index))
+        d["PredA_%"] = f"{round(len(df_a.index) / len(df2.index) * 100, 2)}%"
+        d["PredD_Vars"] = str(len(df_d.index))
+        d["PredD_%"] = f"{round(len(df_d.index) / len(df2.index) * 100, 2)}%"
+        d["PredN_Vars"] = str(len(df_n.index))
+        d["PredN_%"] = f"{round(len(df_n.index) / len(df2.index) * 100, 2)}%"
+        d["PredNA_Vars"] = str(len(df_n[df_n["AIvLD"] > 0.5].index))
+        d["PredNA_%"] = f"{round(len(df_n[df_n['AIvLD'] > 0.5].index) / len(df2.index) * 100, 2)}%"
+        d["PredND_Vars"] = str(len(df_n[df_n["AIvLD"] <= 0.5].index))
+        d["PredND_%"] = f"{round(len(df_n[df_n['AIvLD'] <= 0.5].index) / len(df2.index) * 100, 2)}%"
+        d["PredR_Vars"] = str(len(df_r.index))
+        d["PredR_%"] = f"{round(len(df_r.index) / len(df2.index) * 100, 2)}%"
+
+        final_df.append(d)
 
     final_df = pd.DataFrame(final_df)
     final_df.to_csv(output_file, sep="\t", index=False)
